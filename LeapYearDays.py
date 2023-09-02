47643b8b19092c879953f0f38402ae115d02d1f7
Maitreyee2018 <42126649+Maitreyee2018@users.noreply.github.com>
2023-09-03 01:17:47 +0530

    Create LeapYearDays.py

---
+++ LeapYearDays.py
@@ -0,0 +1,33 @@
+#This program accepts a year and a month and return the number of days of that month.
+#In case of leap year, it prints 29 days for the month of Februrary
+
+def is_leap(year):
+  if year % 4 == 0:
+    if year % 100 == 0:
+      if year % 400 == 0:
+        return True
+      else:
+        return False
+    else:
+      return True
+  else:
+    return False
+
+def days_in_month(year, month):
+  month_days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]  
+  if is_leap(year) == True and month == 2:
+      return 29
+  else:
+      return month_days[month-1]    
+
+year = int(input("Enter a year: "))
+month = int(input("Enter a month: "))
+days = days_in_month(year, month)
+print(days)
+
+
+
+
+
+
+
